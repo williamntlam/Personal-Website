8a6743fff3ea68e908db3e3341962115a88a542b
William Lam <williamntlam@gmail.com>
2024-08-04 21:14:38 -0400

    feat: added blue styling in the text and functional buttons to to send users to my github, linkedin, and devpost author: William Lam<williamntlam@gmail.com>

--- src/components/HomeInfo.tsx
+++ src/components/HomeInfo.tsx
@@ -1,4 +1,6 @@
 import { Typewriter } from 'react-simple-typewriter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const HomeInfo = () => {
   return (
@@ -16,13 +18,33 @@ const HomeInfo = () => {
           />
         </center>
       </h1>
-      <div className="flex-grow flex items-start justify-center w-full pt-8">
-        <p className="text-white leading-relaxed font-montserrat w-2/3 text-center">
-          I'm currently a student at the University of Toronto majoring in
-          Computer Science and Mathematics. I'm deeply interested in software
-          development and AI and I strongly believe in being a positive influence everywhere I go.
-          Through my interests and values, I hope to leave a positive impact on the world and those around me.
+      <p className="text-white leading-relaxed font-montserrat w-2/3 text-center">
+          I'm currently a student at the <span className="text-blue-500"> University of Toronto </span> majoring in
+          <span className="text-blue-500"> Computer Science </span> and <span className="text-blue-500"> Mathematics </span>. 
+          I'm deeply interested in <span className="text-yellow-400"> software development </span> and <span className="text-yellow-400"> AI </span> and I strongly believe in being a 
+          <span className="text-blue-500"> positive influence </span> 
+          everywhere I go. Through my interests and values, I hope to leave a 
+          <span className="text-blue-500"> positive impact </span> 
+          on the world and those around me.
         </p>
+      <div className="flex flex-col items-center pt-8 space-y-4">
+        <div className="flex space-x-4">
+          <a href="https://github.com/williamntlam" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} className="h-10 w-10 text-gray-700 hover:text-white" />
+          </a>
+          <a href="https://www.linkedin.com/in/william-lam-b3a8a2213/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className="h-10 w-10 text-blue-700 hover:text-white" />
+          </a>
+          <a href="" className="bg-black text-white px-4 py-2 rounded border-2 border-white hover:bg-blue-900">
+            Resume
+          </a>
+          <a href="" className="bg-black text-white px-4 py-2 rounded border-2 border-white hover:bg-blue-900">
+            Projects
+          </a>
+          <a href="https://devpost.com/williamntlam" className="bg-black text-white px-4 py-2 rounded border-2 border-white hover:bg-blue-900">
+            Devpost
+          </a>
+        </div>
       </div>
     </div>
   );
