c7dfaa761851134226da95ef3fd94a0cd75056dd
William Lam <williamntlam@gmail.com>
2024-08-05 00:53:24 -0400

    feat: removed colour transition in picture author: William Lam<williamntlam@gmail.com>

--- src/components/HomePicture.tsx
+++ src/components/HomePicture.tsx
@@ -6,7 +6,7 @@ const HomePicture = () => {
       <img
         src={profilePicture}
         alt="Nice picture for the home page."
-        className="w-full h-full object-cover grayscale hover:grayscale-0 transition-filter duration-300"
+        className="w-full h-full object-cover"
       />
     </div>
   );
