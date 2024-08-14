3c89097d4977ed77283fcd336c8a799565956288
William Lam <williamntlam@gmail.com>
2024-08-14 18:53:35 -0400

    feat(home picture): fixed styling issues for hovering picture on mobile device author: William Lam<williamntlam@gmail.com>

--- src/components/HomePicture.tsx
+++ src/components/HomePicture.tsx
@@ -3,7 +3,7 @@ import sunglassesPhoto from "/sunglassesPhoto.jpg";
 
 const HomePicture = () => {
   return (
-    <section className="w-full h-48 md:h-auto relative group">
+    <section className="w-full h-60 md:h-auto relative group">
       <img
         src={profilePicture}
         alt="Nice picture for the home page."
