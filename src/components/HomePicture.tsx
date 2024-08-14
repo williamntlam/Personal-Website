51a0f7df054ef7babedd6a41ef674ef6c4b1d657
William Lam <williamntlam@gmail.com>
2024-08-14 16:47:23 -0400

    feat(home picture): added a funny transition profile picture when the user hovers over the home picture author: William Lam<williamntlam@gmail.com>

--- src/components/HomePicture.tsx
+++ src/components/HomePicture.tsx
@@ -1,12 +1,18 @@
 import profilePicture from "/homePicture.jpg";
+import funnyProfilePicture from "/funnyHomePicture.jpg";
 
 const HomePicture = () => {
   return (
-    <div className="w-full h-48 md:h-auto">
+    <div className="w-full h-48 md:h-auto relative group">
       <img
         src={profilePicture}
         alt="Nice picture for the home page."
         className="w-full h-full object-cover"
+      />
+      <img
+        src={funnyProfilePicture}
+        alt="Another picture on hover."
+        className="w-full h-full object-cover absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
       />
     </div>
   );
