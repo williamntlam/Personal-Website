b3b4c37d3193c2cb523e36554ad8b79bb1c1f8c5
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2024-10-12 22:33:17 -0400

    feat: updated reflections website link due to a overhaul of the reflections website author: William Lam<williamntlam@gmail.com>

--- src/components/HomeInfo.tsx
+++ src/components/HomeInfo.tsx
@@ -34,7 +34,7 @@ const HomeInfo = () => {
         Please feel free to <span className="text-blue-500">explore</span> the
         <span className="text-yellow-400"> links </span> I've{" "}
         <span className="text-blue-500">shared</span> down below - This includes
-        a <span className="text-emerald-400">blogsite</span> - a place where I
+        a <a href="https://williams-reflections-8dt0g1jib-william-lams-projects.vercel.app/"><span className="text-emerald-400">blogsite</span></a> - a place where I
         <span className="text-blue-500"> share</span> my{" "}
         <span className="text-yellow-400">thoughts</span> and{" "}
         <span className="text-yellow-400">reflections</span> from my day to day
@@ -83,7 +83,7 @@ const HomeInfo = () => {
             Devpost
           </a>
           <a
-            href="https://williams-reflections.netlify.app/"
+            href="https://williams-reflections-8dt0g1jib-william-lams-projects.vercel.app/"
             className="bg-black text-white px-4 py-2 rounded border-2 border-white hover:bg-blue-900 text-center mt-4 sm:mt-0"
           >
             Blogsite
